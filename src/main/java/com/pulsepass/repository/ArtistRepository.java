69fe3c6f2320d1cdda29ab5b39143193c094e74f
mariana-ulloa54 <marianajose403@gmail.com>
2026-09-19 17:41:18 -0500

    Arreglo de errores en pulsepass/repository

--- src/main/java/com/pulsepass/repository/ArtistRepository.java
+++ src/main/java/com/pulsepass/repository/ArtistRepository.java
@@ -1,9 +1,9 @@
 package com.pulsepass.repository;
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.pulsepass.domain.Artist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
 
